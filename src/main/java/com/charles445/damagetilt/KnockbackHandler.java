4b8402c7f9b1b12b6cbaf90d64886b3d076112fa
Charles445 <charlesd@wenzel.com>
2021-02-09 21:23:40 -0600

    Fix Non-ServerPlayerEntity Crash
    A simple instanceof...

--- src/main/java/com/charles445/damagetilt/KnockbackHandler.java
+++ src/main/java/com/charles445/damagetilt/KnockbackHandler.java
@@ -4,6 +4,7 @@ import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
 
 public class KnockbackHandler
 {
@@ -12,7 +13,7 @@ public class KnockbackHandler
 		if(entity instanceof PlayerEntity)
 		{
 			PlayerEntity player = (PlayerEntity)entity;
-			if(!player.world.isClient)
+			if(!player.world.isClient && player instanceof ServerPlayerEntity)
 			{
 				//Server side, send packet
 				PacketByteBuf data = new PacketByteBuf(Unpooled.buffer());
