e51cc93c09c7df1a832c954a4a2b88e05c53ae3c
Charles445 <charlesd@wenzel.com>
2021-02-09 21:31:26 -0600

    Fix Non-ServerPlayerEntity Crash
    A simple instanceof...

--- src/main/java/com/charles445/damagetilt/DamageTilt.java
+++ src/main/java/com/charles445/damagetilt/DamageTilt.java
@@ -7,7 +7,7 @@ public class DamageTilt implements ModInitializer
 {
 	public static final String MODID = "damagetilt";
 	public static final String NAME = "Damage Tilt";
-	public static final String VERSION = "0.1.1";
+	public static final String VERSION = "0.1.2";
 	
 	public static final Identifier PLAY_YAW_PACKET_ID = new Identifier(DamageTilt.MODID, "attackedatyaw");
 	
